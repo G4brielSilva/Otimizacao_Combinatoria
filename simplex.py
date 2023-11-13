525aea5033149d42dcf58ba578aa3bc328eb1c28
daviguindani <davigpvieira@gmail.com>
2023-11-13 19:33:33 -0300

    renomeando algumas coisas

--- simplex.py
+++ simplex.py
@@ -1,47 +1,50 @@
 import pandas as pd
 
 with open("./problema_lp/problema3.txt", "r") as arq:
-    content = arq.read()
-lines = content.split("\n")
+    conteudo = arq.read()
+linhas = conteudo.split("\n")
 
 # lidando com a maximização
 listaSinalTrocado = []
-if lines[0].split(" ")[2] == "1":
-    for i in range(len(lines[1].split(" "))):
-        listaSinalTrocado = lines[1].split(" ")
+if linhas[0].split(" ")[2] == "1":
+    for i in range(len(linhas[1].split(" "))):
+        listaSinalTrocado = linhas[1].split(" ")
         for i in range(len(listaSinalTrocado)):
             listaSinalTrocado[i] = str(float(listaSinalTrocado[i]) * -1)
-    lines[1] = " ".join(listaSinalTrocado)
+    linhas[1] = " ".join(listaSinalTrocado)
 
 # lidando com termos independentes negativos
-for i in range(2, int(lines[0][0]) + 2):
-    if int(lines[i].split(" ")[int(content[2]) + 1]) < 0:
+for i in range(2, int(linhas[0][0]) + 2):
+    if int(linhas[i].split(" ")[int(conteudo[2]) + 1]) < 0:
         linha = [
-            float(item) * -1 if index != len(lines[i].split(" ")) - 2 else item
-            for index, item in enumerate(lines[i].split(" "))
+            float(item) * -1 if index != len(linhas[i].split(" ")) - 2 else item
+            for index, item in enumerate(linhas[i].split(" "))
         ]
-        if linha[int(lines[0].split(" ")[1])] == ">=":
-            linha[int(lines[0].split(" ")[1])] = "<="
-        if linha[int(lines[0].split(" ")[1])] == "<=":
-            linha[int(lines[0].split(" ")[1])] = ">="
+
+        sinal = int(linhas[0].split(" ")[1])
+
+        if linha[sinal] == ">=":
+            linha[sinal] = "<="
+        if linha[sinal] == "<=":
+            linha[sinal] = ">="
         linha = [str(elemento) for elemento in linha]
         linha = " ".join(linha)
-        lines[i] = linha
+        linhas[i] = linha
 
 # montando o tableau inicial
 listona = []
 lista = []
-for i in range(1, int(content[0]) + 2):
+for i in range(1, int(conteudo[0]) + 2):
     j = 0
-    for j in range(int(content[2])):
-        lista.append(float(lines[i].split(" ")[j]))
+    for j in range(int(conteudo[2])):
+        lista.append(float(linhas[i].split(" ")[j]))
     if i > 1:
-            lista.append(lines[i].split(" ")[j + 2])
+        lista.append(linhas[i].split(" ")[j + 2])
     listona.append(lista)
     lista = []
 
 df = pd.DataFrame(listona)
 listona = []
 df = df.fillna(0)
-df[int(content[2])] = df[int(content[2])].astype(float)
+df[int(conteudo[2])] = df[int(conteudo[2])].astype(float)
 print(df)
