88752ff7c20a4e3a1c6c86dce582f908305edabf
daviguindani <davigpvieira@gmail.com>
2023-11-19 10:08:57 -0300

    adicionando variaveis artificiais

--- criar_tableau.py
+++ criar_tableau.py
@@ -14,9 +14,7 @@ def criar_tableau(
     tableau = np.zeros((num_restricoes + 1, num_variaveis + num_restricoes + 1))
 
     # Configurar a função objetivo na primeira linha do tableau
-    tableau[0, :num_variaveis] = (
-        -coeficientes_funcao_objetivo if otimizacao else coeficientes_funcao_objetivo
-    )  # Garantindo que o caso vai cair pra minimização sempre
+    tableau[ 0, :num_variaveis ] = -coeficientes_funcao_objetivo if otimizacao else coeficientes_funcao_objetivo # Garantindo que o caso vai cair pra minimização sempre
 
     for i in range(num_restricoes):
         if termos_independentes[i] < 0:
@@ -26,50 +24,54 @@ def criar_tableau(
                 operations[i] = ">="
             elif operations[i] == ">=":
                 operations[i] = "<="
-
+        if operations[i] == "==":
+            tableau[i + 1, num_variaveis + i] = 1  
+            tableau[i + 1, -1] = termos_independentes[i]
+    
     for termo in termos_independentes:
-        if termo < 0:
+        if termo <0:
             return None
 
     rows_need_var_art = []
-    M = 1000  
-
     # Configurar as restrições e os termos independentes
     for i in range(num_restricoes):
         tableau[i + 1, :num_variaveis] = coeficientes_restricoes[i]
 
-        # Adicionando variáveis de folga e artificiais com Big M
+        # Adicionando variáveis de folga
         if operations[i] == "<=":
             tableau[i + 1, num_variaveis + i] = 1
-            tableau[i + 1, -1] = termos_independentes[i]
         elif operations[i] == ">=":
             tableau[i + 1, num_variaveis + i] = -1
-            tableau[i + 1, -1] = termos_independentes[i]
-        else:  # "=="
-            tableau[i + 1, num_variaveis + i] = M  # Variável artificial com penalização M
-            tableau[i + 1, -1] = M * termos_independentes[i]
+        else:
+            tableau[i + 1, num_variaveis + i] = 0
 
-        if rows_need_var_art:
-            var_art = np.zeros((len(tableau), len(tableau)))
+        if tableau[i + 1, num_variaveis + i] != 1:
+            rows_need_var_art.append(i + 1)
 
-            for i in range(len(var_art)):
-                if i in rows_need_var_art:
-                    var_art[0][i] = 1
-                    var_art[i][i] = 1
-                # array_atualizado = np.insert(array_original, array_original.shape[1], nova_coluna, axis=1)
+        tableau[i + 1, -1] = termos_independentes[i]
 
-            b = tableau[:, -1]
-            var_art = np.array(var_art)
-            # Inserir as colunas de variáveis artificiais antes da última coluna do tableau
-            tableau = tableau[:, :-1]
-            tableau[0] = 0
-            # print(var_art)
-            tableau = np.concatenate((tableau, var_art), axis=1)
-            tableau = np.concatenate((tableau, b.reshape(-1, 1)), axis=1)
+    if rows_need_var_art:
+        var_art = np.zeros((len(tableau), len(tableau)))
 
-            indices = np.where(tableau[0] == 1)[0]
-            for indice in indices:
-                row = np.where(tableau[:, indice] == 1)[0][1]
-                tableau[0] = tableau[0] - tableau[row]
+        for i in range(len(var_art)):
+            if i in rows_need_var_art:
+                var_art[0][i] = 1
+                var_art[i][i] = 1
+            # array_atualizado = np.insert(array_original, array_original.shape[1], nova_coluna, axis=1)
 
-        return tableau
+
+        b = tableau[: , -1]
+        var_art = np.array(var_art)
+        # Inserir as colunas de variáveis artificiais antes da última coluna do tableau
+        tableau = tableau[:, :-1]
+        tableau[0] = 0
+        # print(var_art)
+        tableau = np.concatenate((tableau, var_art), axis=1)
+        tableau = np.concatenate((tableau, b.reshape(-1, 1)), axis=1)
+
+        indices = np.where(tableau[0] == 1)[0]
+        for indice in indices:
+            row = np.where(tableau[:, indice] == 1)[0][1]
+            tableau[0] = tableau[0] - tableau[row]
+
+    return tableau
